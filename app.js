323d70b7fa02b630ed4388951c04a95ab671b202
StylishCS <118624912+StylishCS@users.noreply.github.com>
2023-07-09 22:25:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("upload"));
+app.use(express.static("uploads"));
 // app.use(cors());
 
 app.use('/', homeRouter);
